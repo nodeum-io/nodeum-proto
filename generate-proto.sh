9632b1f3c7f307a484d19b22da9b53b70437459f
Thomas Chandelle <thomas.chandelle@mt-c.com>
2022-04-08 11:19:20 +0200

    Add tools to generate locally with asdf

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,7 +1,15 @@
-mkdir -p $GOPATH/src/github.com/nodeum-io
-ln -svfT /workspaces/nodeum-plugins $GOPATH/src/github.com/nodeum-io/nodeum-plugins
+# If it fails:
+#  - `go install` the packages from `.default-golang-pkgs`
+#  - Run `asdf reshim golang`
 
-PROTO_PATH="--proto_path=$GOPATH/src:$GOPATH/src/github.com/googleapis/googleapis:."
+cd "`dirname \"$0\"`"
+
+GOPATH=`go env GOPATH`
+
+mkdir -p $GOPATH/src/github.com/nodeum-io
+ln -svf $PWD $GOPATH/src/github.com/nodeum-io/nodeum-plugins
+
+PROTO_PATH="--proto_path=$GOPATH/src:$GOPATH/pkg/mod/github.com/googleapis/googleapis@v0.0.0-20220408000435-23a2d2961f24:."
 
 find . -name '*.type.proto' -exec protoc $PROTO_PATH --go_out=$GOPATH/src {} \;
 find . -name '*.micro.proto' -exec protoc $PROTO_PATH --go_out=$GOPATH/src --micro_out=$GOPATH/src {} \;
